d9ddf6a40bf699de5697309175af9acd4fcd2415
lagarto22 <lagarto22@gmail.com>
2018-11-28 09:52:52 -0300

    prueba de local a repo2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    SISTEMA DE MEMOS
+                    MEMOS
                 </div>
 
             <!--    <div class="links">
